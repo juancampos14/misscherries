5b1fd7e450e08b1b8145dd694e8e86cfc64c2d2b
juan.campos <juan.campos@brooktec.com>
2024-03-11 12:44:18 +0100

    cambios para que el titulo de la categoria o subcategoria se vea correctamente

--- wp-content/themes/misscherries/archive.php
+++ wp-content/themes/misscherries/archive.php
@@ -27,87 +27,88 @@ get_header();
             'order'    => 'ASC',
         ));
         ?>
-        <h2 class="title-product"><?php echo $current_term->name; ?></h2>
-        <?php
-        if (empty($subcategories)) {
-            ?>
-            <div class="container">
-                <div class="row display-card">
-                    <?php
-                    $args = array(
-                        'post_type' => 'product',
-                        'posts_per_page' => -1,
-                        'tax_query' => array(
-                            array(
-                                'taxonomy' => $taxonomy,
-                                'field' => 'id',
-                                'terms' => $current_term->term_id,
+        <div class="container">
+            <h2 class="title-product"><?php echo $current_term->name; ?></h2>
+            <?php
+            if (empty($subcategories)) {
+                ?>
+                    <div class="row display-card">
+                        <?php
+                        $args = array(
+                            'post_type' => 'product',
+                            'posts_per_page' => -1,
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => $taxonomy,
+                                    'field' => 'id',
+                                    'terms' => $current_term->term_id,
+                                ),
                             ),
-                        ),
-                    );
-        
-                    $query = new WP_Query($args);
-        
-                    if ($query->have_posts()) :
-                        while ($query->have_posts()) : $query->the_post();
-                            ?>
-                            <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
-                                <div class="block-card">
-                                    <div class="img-card">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php
-                                            if (has_post_thumbnail()) {
-                                                the_post_thumbnail('full', array('class' => 'img-item', 'alt' => get_the_title()));
-                                            }
-                                            ?>
-                                        </a>
-                                    </div>
-                                    <div class="card-footer">
-                                        <div class="background-footer-block1-card">
-                                            <div class="background-footer-block2-card">
-                                                <p class="text-footer-card"><?php the_title(); ?></p>
+                        );
+            
+                        $query = new WP_Query($args);
+            
+                        if ($query->have_posts()) :
+                            while ($query->have_posts()) : $query->the_post();
+                                ?>
+                                <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                                    <div class="block-card">
+                                        <div class="img-card">
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php
+                                                if (has_post_thumbnail()) {
+                                                    the_post_thumbnail('full', array('class' => 'img-item', 'alt' => get_the_title()));
+                                                }
+                                                ?>
+                                            </a>
+                                        </div>
+                                        <div class="card-footer">
+                                            <div class="background-footer-block1-card">
+                                                <div class="background-footer-block2-card">
+                                                    <p class="text-footer-card"><?php the_title(); ?></p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        <?php
-                        endwhile;
-                    endif;
-        
-                    wp_reset_postdata();
-                    ?>
-                </div>
-            </div>
-        <?php } ?>
-
-        <div class="row display-card">
-            <?php foreach ($subcategories as $subcategory) : ?>
-                <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
-                    <div class="block-card">
-                        <a href="<?php echo esc_attr(get_term_link($subcategory, $taxonomy)); ?>">
-                            <div class="img-card">
-                                <?php
-                                $id_image = get_field('imagen-taxonomy', $subcategory);
-                                echo wp_get_attachment_image($id_image, 'full', false, ['class' => 'img-item imagen-taxonomy']);
-                                ?>
-                            </div>
-                            <div class="card-footer">
-                                <div class="background-footer-block1-card">
-                                    <div class="background-footer-block2-card">
-                                        <p class="text-footer-card"><?php echo $subcategory->name; ?></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
+                            <?php
+                            endwhile;
+                        endif;
+            
+                        wp_reset_postdata();
+                        ?>
                     </div>
                 </div>
-            <?php endforeach; ?>
+            <?php } ?>
+
+            <div class="row display-card">
+                <?php foreach ($subcategories as $subcategory) : ?>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                        <div class="block-card">
+                            <a href="<?php echo esc_attr(get_term_link($subcategory, $taxonomy)); ?>">
+                                <div class="img-card">
+                                    <?php
+                                    $id_image = get_field('imagen-taxonomy', $subcategory);
+                                    echo wp_get_attachment_image($id_image, 'full', false, ['class' => 'img-item imagen-taxonomy']);
+                                    ?>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="background-footer-block1-card">
+                                        <div class="background-footer-block2-card">
+                                            <p class="text-footer-card"><?php echo $subcategory->name; ?></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
     <?php } else { // Si es una subcategoría, mostrar productos
         ?>
+        <div class="container ">
         <h2 class="title-product"><?php echo $current_term->name; ?></h2>
-        <div class="container">
             <div class="row display-card">
                 <?php
                 $args = array(
